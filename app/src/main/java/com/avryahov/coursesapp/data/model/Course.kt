a5041ad318d408db43ef8287c73446e66695bcf4
avrjakhov <a.v.rjakhov@yandex.ru>
2026-01-18 13:55:16 +0500

    fix: update Course data types
    — price as Int,
    — rate as Float,
    — remove redundant priceAsInt

--- app/src/main/java/com/avryahov/coursesapp/data/model/Course.kt
+++ app/src/main/java/com/avryahov/coursesapp/data/model/Course.kt
@@ -3,20 +3,13 @@ package com.avryahov.coursesapp.data.model
 import androidx.annotation.DrawableRes
 
 data class Course(
-    val id: String,
+    val id: Int,
     val title: String,
     val text: String,
-    val price: String,
-    val rate: String,
+    val price: Int,
+    val rate: Float,
     val startDate: String,
     val hasLike: Boolean,
     val publishDate: String,
     @field:DrawableRes val bannerResId: Int
-) {
-    val priceAsInt: Int
-        get() = price
-            .replace(" ", "")
-            .replace("₽", "")
-            .replace("\u00A0", "")
-            .toIntOrNull() ?: 0
-}
+)

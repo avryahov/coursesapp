2c19fc14e136f0a3e284dc289aa3d864b2862323
avrjakhov <a.v.rjakhov@yandex.ru>
2026-01-18 12:33:26 +0500

    feat: add HomeViewModel to load courses from repository

--- app/src/main/java/com/avryahov/coursesapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/avryahov/coursesapp/presentation/home/HomeViewModel.kt
@@ -1,1 +1,30 @@
-// Auto-generated stub for com/avryahov/coursesapp/presentation/home/HomeViewModel.kt
+package com.avryahov.coursesapp.presentation.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.avryahov.coursesapp.data.model.Course
+import com.avryahov.coursesapp.data.repository.CourseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val courseRepository: CourseRepository
+) : ViewModel() {
+
+    private val _courses = MutableStateFlow<List<Course>>(emptyList())
+    val courses: StateFlow<List<Course>> = _courses
+
+    init {
+        loadCourses()
+    }
+
+    private fun loadCourses() {
+        viewModelScope.launch {
+            _courses.value = courseRepository.getCourses()
+        }
+    }
+}
